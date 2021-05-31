789670983c1b30523b93ae2743fe2dd34a4291dd
Cameron Whyte <public@cameronwhyte.me>
2021-05-31 01:48:31 +0100

    Update install.sh

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -82,7 +82,7 @@ cloneconfig() {
 	git clone https://github.com/Redstoneguy129/lunarvim.git ~/.config/nvim
 	# mv $HOME/.config/nvim/init.lua $HOME/.config/nvim/init.lua.tmp
 	# mv $HOME/.config/nvim/utils/init.lua $HOME/.config/nvim/init.lua
-	nvim -u $HOME/.config/nvim/init.lua +PackerInstall +qa
+	nvim -u $HOME/.config/nvim/init.lua +PackerInstall -c "sleep 5 | qa"
 	# rm $HOME/.config/nvim/init.lua
 	# mv $HOME/.config/nvim/init.lua.tmp $HOME/.config/nvim/init.lua
 }
